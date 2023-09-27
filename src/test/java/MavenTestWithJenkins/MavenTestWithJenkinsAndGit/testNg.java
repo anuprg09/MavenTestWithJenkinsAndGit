030721ebe6b86a34ef2d47aa9c344ba7e872bed3
anupr <anu.prg09@gmail.com>
2023-09-27 07:58:22 +0530

    this is my Test project for GIT and Jenkins Integration

---
+++ src/test/java/MavenTestWithJenkins/MavenTestWithJenkinsAndGit/testNg.java
@@ -0,0 +1,36 @@
+package MavenTestWithJenkins.MavenTestWithJenkinsAndGit;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertNotNull;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class testNg {
+  WebDriver driver;
+	@Test
+  public void CheckIfGooglePageIsOpen() {
+  WebElement searchTextBox = driver.findElement(By.name("q"));
+  assertNotNull(searchTextBox);
+  searchTextBox.sendKeys("This is test");
+  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+	  driver.get("https://www.google.com");
+	  driver.manage().window().fullscreen();
+  }
+
+  @AfterTest
+  public void afterTest() {
+  driver.quit();
+  }
+
+}
